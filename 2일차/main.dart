b861d1f4ffd61f1c7448e68989df7a0859f88338
jiae <kga9002@naver.com>
2025-07-08 15:15:56 +0900

    [UPDATE] 반복문 조건문 퀴즈

--- 2일차/main.dart
+++ 2일차/main.dart
@@ -1,7 +1,24 @@
 void main() {
-  for (var dan = 1; dan < 10; dan++) {
-    for (var number = 1; number < 10; number++) {
-      print("$dan X $number = ${number * dan}");
+  var text = "";
+  for (var i = 0; i < 7; i++) {
+    String singleLine = "";
+    for (var j = 0; j < 7; j++) {
+      if (i == 0 || i == 6)
+        singleLine += "-";
+      else if (j == 0 || j == 6)
+        singleLine += "|";
+      else if (i == j || i + j == 6)
+        singleLine += "X";
+      else
+        singleLine += "O";
+    }
+    text += "$singleLine\n";
+  }
+  print(text);
+
+  for (var number = 2; number < 10; number += 2) {
+    for (var number2 = 1; number2 < 10; number2++) {
+      print("$number X $number2 = ${number * number2}");
     }
   }
 }

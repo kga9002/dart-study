8e4d00892786fce752aad1905d8e5e6b2fc8e94d
jiae <kga9002@naver.com>
2025-07-09 13:45:53 +0900

    [UPDATE] map,set quiz

--- 3일차/main.dart
+++ 3일차/main.dart
@@ -1,28 +1,27 @@
-import 'dart:math';
-
 void main() {
-  List list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  list.insert(0, 9);
-  print(list);
+  Set<String> korean1 = {"가", "나", "다", "라", "마", "바", "사"};
+  Set<String> korean2 = {"라", "마", "바", "사", "아", "자", "차", "카", "파", "타", "하"};
+  Set newKorean = {};
+  newKorean.addAll(korean1);
+  newKorean.addAll(korean2);
 
-  //==================================================
+  print(newKorean);
+  //교집합,,합집합,,,차집합을 새로운 세트로
+  Set intersection = korean1.intersection(korean2);
+  print(intersection);
 
-  print(list[6]);
+  Set difference = korean1.difference(korean2);
+  print(difference);
 
-  //==================================================
-  List newList = ["1", "2", "3"];
-  list.addAll(newList);
-  print(list);
+  Set union = korean1.union(korean2);
+  print(union);
 
-  //==================================================
-  List stringList = ["Apple", "Mango", "Banana", "Kiwi", "Grape"];
-  stringList.sort();
-  print(stringList);
+  //==============================================
 
-  //==================================================
-  List numberList = [];
-  for (int i = 0; i < 10; i++) {
-    numberList.add(Random().nextInt(100));
-  }
-  print(numberList);
+  Map shop = {"apple": 10, "banana": 5, "mango": 3};
+  var mangoCnt = shop.containsKey("mango") ? shop["mango"] : 0;
+  print("이 상점에서는 망고를 $mangoCnt개 판매하고 있습니다.");
+
+  shop['mango'] -= 2;
+  print("현재 망고 갯수 : ${shop["mango"]}");
 }

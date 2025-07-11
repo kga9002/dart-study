54f0b9b73fdbd14847b17b1cc6f9bfce5dbd50c8
jiae <kga9002@naver.com>
2025-07-11 17:18:25 +0900

    [UPDATE] 5일차 퀴즈 풀이

--- 5일차/main.dart
+++ 5일차/main.dart
@@ -1,11 +1,30 @@
+class NegativeDivisorException implements Exception {
+  @override
+  String toString() {
+    return "Zero divisor is not allowed";
+  }
+}
+
 void main() {
   int num1 = 1;
   int num2 = 0;
   int result = 0;
 
+  int calc(int left, int right) {
+    int result = 0;
+
+    if (right == 0) {
+      throw NegativeDivisorException();
+    } else {
+      result = left ~/ right;
+    }
+
+    return result;
+  }
+
   try {
-    result = num1 ~/ num2;
-  } on UnsupportedError {
+    result = calc(num1, num2);
+  } on NegativeDivisorException {
     print("num2의 값은 0입니다.");
     num2 = 1;
     print("num2의 값을 1로 바꾸고 재시도 합니다.");
---
+++ 5일차/quiz3.dart
@@ -0,0 +1,20 @@
+// NoStringException 이라는 에러 클래스를 생성한 후,
+// main 함수에 num 이라는 변수에 10 을 넣어주세요.
+// 이후 num의 타입이 string 이 아닐 경우,
+// NoStringException 클래스를 통해 다음의 내용이 출력되게 구현하세요.
+// [해당 값은 문자열이 아닙니다.]
+
+class NoStringException implements Exception {
+  @override
+  String toString() {
+    return "해당 값은 문자열이 아닙니다.";
+  }
+}
+
+void main() {
+  int num = 10;
+
+  if (num.runtimeType != "String") {
+    throw NoStringException();
+  }
+}

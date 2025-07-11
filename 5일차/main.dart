785ae67d23222b822d97784794b23647ddc4779d
jiae <kga9002@naver.com>
2025-07-11 14:29:40 +0900

    [UPDATE] bg 함수 작업

--- 5일차/main.dart
+++ 5일차/main.dart
@@ -1,32 +1,34 @@
-class Box<T> {
-  late dynamic item;
+// void main()  {
+//   print(1 + 1);
 
-  static int changeCount = 0;
-  int totalCount = 0;
+//    Future.delayed(Duration(seconds: 1), () => print("hello"));
+//   print(2 + 2);
+// }
 
-  Box(T givenItem) : item = givenItem;
-
-  dynamic get() {
-    return item;
-  }
-
-  void set(dynamic newItem) {
-    item = newItem;
-    changeCount = changeCount + 1;
-    totalCount = totalCount + 1;
+doBG(int time, Flag finished) {
+  if (time > 0) {
+    print("$time 초 남았습니다");
+    Future.delayed(Duration(seconds: 1), () => doBG(time - 1, finished));
+  } else {
+    print("밥준비완.");
+    finished.activated = true;
   }
 }
 
-void main() {
-  var intBox = Box(1);
-  var newBox2 = Box("참치");
+class Flag {
+  late bool _flag;
 
-  print(intBox.get());
+  Flag(bool givenFlag) : _flag = givenFlag;
+  bool get activated => _flag;
+  set activated(bool givenFlag) => _flag = givenFlag;
+}
 
-  intBox.set(2);
-  newBox2.set(1);
-
-  print(Box.changeCount);
-  print(intBox.totalCount);
-  print(Box.changeCount);
+void main() async {
+  var finished = Flag(false);
+  print("order");
+  await doBG(5, finished);
+  while (!finished.activated) {
+    await Future.delayed(Duration(seconds: 1));
+  }
+  print("eat");
 }

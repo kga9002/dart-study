bc23651678f061c9339e178cb1d7c82c80fa9153
jiae <kga9002@naver.com>
2025-07-11 16:30:40 +0900

    [UPDATE] finally 학습

--- 5일차/main.dart
+++ 5일차/main.dart
@@ -1,45 +1,18 @@
-doBG(int time, var jobEnd) {
-  if (time > 0) {
-    print("$time 초 남았습니다.");
-    Future.delayed(Duration(seconds: 1), () => doBG(time - 1, jobEnd));
-  } else {
-    print("밥이 준비되었습니다.");
-    jobEnd.activated = true;
+void main() {
+  int num1 = 1;
+  int num2 = 0;
+  int result = 0;
+
+  try {
+    result = num1 ~/ num2;
+  } on UnsupportedError {
+    print("num2의 값은 0입니다.");
+    num2 = 1;
+    print("num2의 값을 1로 바꾸고 재시도 합니다.");
+    result = num1 ~/ num2;
+  } catch (e) {
+    print("알수없는 에러");
+  } finally {
+    print(result);
   }
-}
-
-class Flag {
-  late bool _flag;
-
-  Flag(bool givenFlag) : _flag = givenFlag;
-  bool get activated => _flag;
-  set activated(bool givenFlag) => _flag = givenFlag;
-}
-
-Future<String> serveCustomer() async {
-  print("serveCustomer: 고객 주문 대기중");
-  var customerOrder = await simulateCustomerOrder();
-  print("serveCustomer: 고객의 주문 '$customerOrder' 를 받음");
-
-  return customerOrder;
-}
-
-Future<String> simulateCustomerOrder() {
-  return Future.delayed(Duration(seconds: 2), () => '아이스 커피');
-}
-
-void main() async {
-  var finished = Flag(false);
-
-  print("main: 메인 함수 실행");
-  await doBG(5, finished);
-
-  var customerOrder = await serveCustomer();
-  print("main: 고객의 주문을 받음 : $customerOrder");
-
-  while (finished.activated == false) {
-    await Future.delayed(Duration(seconds: 1));
-  }
-
-  print("main: 메인 함수 종료");
 }

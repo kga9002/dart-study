1bfc61ee17ad852b822598b976cb77d200ea752d
jiae <kga9002@naver.com>
2025-07-15 16:02:13 +0900

    [UPDATE] http client

--- 7일차/main.dart
+++ 7일차/main.dart
@@ -1,60 +1,121 @@
 import "dart:io";
+import 'dart:convert';
 
-void main() async {
-  var ip = InternetAddress.loopbackIPv4;
-  var port = 4040;
-  var server = await HttpServer.bind(ip, port);
+void printHttpServerActivated(HttpServer server) {
+  var ip = server.address.address;
+  var port = server.port;
 
-  print("\$ server activated - ${server.address.address}:${server.port}");
+  print("\$ Server Activated in $ip:$port");
+}
+
+void printHttpRequestInfo(HttpRequest request) async {
+  var ip = request.connectionInfo!.remoteAddress.address;
+  var port = request.connectionInfo!.remotePort;
+  var method = request.method;
+  var path = request.uri.path;
+
+  print("\$ $method $path from $ip:$port");
+
+  if (request.headers.contentLength != -1) {
+    print("\> content-type : ${request.headers.contentType}");
+    print("\> content-length : ${request.headers.contentLength}");
+  }
+}
+
+void httpGetHandler(HttpRequest request) async {
+  if (request.uri.path == '/') {
+    var content = "Hello, World!";
+    requestResponseHandler(request, content);
+  } else if (request.uri.path.contains('/add')) {
+    var vars = request.uri.path.split(',');
+    var result = int.parse(vars[1]) + int.parse(vars[2]);
+    var content = "${vars[1]} + ${vars[2]} = $result";
+
+    requestResponseHandler(request, content);
+  } else if (await File(request.uri.path.substring(1)).exists() == true) {
+    var file = File(request.uri.path.substring(1));
+    var content = await file.readAsString();
+
+    requestResponseHandler(request, content);
+  } else {
+    var content = 'Unsupported URI';
+
+    requestResponseHandler(request, content);
+  }
+
+  await request.response.close();
+}
+
+void httpPutHandler(var addr, var port, HttpRequest request) async {
+  var content = await utf8.decoder.bind(request).join();
+  var file = await File(request.uri.path.substring(1)).openWrite();
+  print("\> content         : ${content}");
+  file
+    ..write(content)
+    ..close();
+  content = 'http://$addr:$port${request.uri.path} created';
+  requestResponseHandler(request, content);
+  await request.response.close();
+}
+
+void httpPostHandler(HttpRequest request) async {
+  var content = await utf8.decoder.bind(request).join();
+  var product = content.split("=");
+  print("\> content        : ${content}");
+  content = "Product '${product[1]}' accepted";
+  requestResponseHandler(request, content);
+  await request.response.close();
+}
+
+void httpDeleteHandler(HttpRequest request) async {
+  var filename = request.uri.path.substring(1);
+  if (await File(filename).exists() == true) {
+    var content = "$filename deleted";
+    File(filename).deleteSync();
+    requestResponseHandler(request, content);
+  } else {
+    var content = "$filename not found";
+    requestResponseHandler(request, content);
+  }
+  await request.response.close();
+}
+
+void requestResponseHandler(HttpRequest request, String content) async {
+  request.response
+    ..headers.contentType = ContentType('text', 'plain', charset: "utf-8")
+    ..headers.contentLength = content.length
+    ..statusCode = HttpStatus.notFound
+    ..write(content);
+}
+
+Future main() async {
+  var server = await HttpServer.bind(InternetAddress.loopbackIPv4, 3000);
+
+  printHttpServerActivated(server);
 
   await for (HttpRequest request in server) {
+    printHttpRequestInfo(request);
+
     try {
-      if (request.uri.path == "/") {
-        print("http response is 'Hello world'");
-        print("send '200 ok'");
-
-        request.response
-          ..statusCode = HttpStatus.ok
-          ..write("Hello World");
-
-        await request.response.close();
-      } else if (request.uri.path.contains("/add")) {
-        print("http response is result of 'add' operation");
-        print("send '200 ok'");
-
-        var varList = request.uri.path.split(",");
-        var result = int.parse(varList[1]) + int.parse(varList[2]);
-
-        request.response
-          ..statusCode = HttpStatus.ok
-          ..write("${varList[1]} + ${varList[2]} = $result");
-        await request.response.close();
-      } else if (await File(request.uri.path.substring(1)).exists() == true) {
-        print("https response is ${request.uri.path} file transfer");
-        print("send '200 ok'");
-
-        var file = File(request.uri.path.substring(1));
-        var fileContent = await file.readAsString();
-
-        request.response
-          ..statusCode = HttpStatus.ok
-          ..headers.contentType = ContentType('text', "plain", charset: "utf-8")
-          ..write(fileContent);
-
-        await request.response.close();
-      } else if (request.uri.path.contains("/dan")) {
-        var varList = request.uri.path.split(",");
-        var dan = int.parse(varList[1]);
-
-        request.response.statusCode = HttpStatus.ok;
-        for (var i = 1; i < 10; i++) {
-          request.response.write("$dan * $i = ${i * dan}\n");
-        }
-        await request.response.close();
+      switch (request.method) {
+        case "GET":
+          httpGetHandler(request);
+          break;
+        case "PUT":
+          httpPutHandler(server.address.address, server.port, request);
+          break;
+        case "POST":
+          httpPostHandler(request);
+          break;
+        case "DELETE":
+          httpDeleteHandler(request);
+          break;
+        default:
+          print("Unsupported http method");
       }
     } catch (e) {
-      print("exception in http request processing");
       print(e);
+      ;
     }
   }
 }
